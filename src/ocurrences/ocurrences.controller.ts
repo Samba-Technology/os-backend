eb5046fc984f4e2c8a8d49feb4165d006d794ffd
Frooszy <thecryzin@gmail.com>
2024-08-22 16:52:08 -0300

    general improvements

--- src/ocurrences/ocurrences.controller.ts
+++ src/ocurrences/ocurrences.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { OcurrencesService } from './ocurrences.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
@@ -9,77 +20,108 @@ import { OcurrenceListEntity } from './entities/ocurrenceList.entity';
 
 @Controller('ocurrences')
 export class OcurrencesController {
-    constructor(private readonly ocurrencesService: OcurrencesService) { }
+  constructor(private readonly ocurrencesService: OcurrencesService) {}
 
-    @Post()
-    @UseGuards(JwtAuthGuard)
-    @ApiCreatedResponse({ type: OcurrenceEntity })
-    async create(
-        @Body() { description, level, students }: OcurrenceDto,
-        @Request() req: RequestWithUser
-    ) {
-        return this.ocurrencesService.create(description, level, students, req.user.id)
-    }
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiCreatedResponse({ type: OcurrenceEntity })
+  async create(
+    @Body() { description, level, students }: OcurrenceDto,
+    @Request() req: RequestWithUser,
+  ) {
+    return this.ocurrencesService.create(
+      description,
+      level,
+      students,
+      req.user.id,
+    );
+  }
 
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceListEntity })
-    async getOcurrences(
-        @Request() req: RequestWithUser,
-        @Query() query: any
-    ) {
-        return await this.ocurrencesService.findOcurrences(req.user.id, req.user.role, parseInt(query.page), parseInt(query.limit), query.isArchive, query.queryStudent, parseInt(query.queryUser))
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceListEntity })
+  async getOcurrences(@Request() req: RequestWithUser, @Query() query: any) {
+    return await this.ocurrencesService.findOcurrences(
+      req.user.id,
+      req.user.role,
+      parseInt(query.page),
+      parseInt(query.limit),
+      query.isArchive,
+      query.queryStudent,
+      parseInt(query.queryUser),
+      query.queryClass,
+    );
+  }
 
-    @Put(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceEntity })
-    async assumeOcurrence(
-        @Param('id') id: string,
-        @Request() req: RequestWithUser
-    ) {
-        return await this.ocurrencesService.assumeOcurrence(parseInt(id), req.user.id, req.user.role)
-    }
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceEntity })
+  async assumeOcurrence(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+  ) {
+    return await this.ocurrencesService.assumeOcurrence(
+      parseInt(id),
+      req.user.id,
+      req.user.role,
+    );
+  }
 
-    @Post(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceEntity })
-    async disptachOcurrence(
-        @Param('id') id: string,
-        @Request() req: RequestWithUser,
-        @Body() { dispatch }: OcurrenceDto
-    ) {
-        return await this.ocurrencesService.dispatchOcurrence(parseInt(id), req.user.role, dispatch)
-    }
+  @Post(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceEntity })
+  async disptachOcurrence(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+    @Body() { dispatch }: OcurrenceDto,
+  ) {
+    return await this.ocurrencesService.dispatchOcurrence(
+      parseInt(id),
+      req.user.role,
+      dispatch,
+    );
+  }
 
-    @Post('/edit/:id')
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceEntity })
-    async editOcurrence(
-        @Param('id') id: string,
-        @Request() req: RequestWithUser,
-        @Body() { description, level, students }: OcurrenceDto
-    ) {
-        return await this.ocurrencesService.editOcurrence(parseInt(id), req.user.id, description, level, students)
-    }
+  @Post('/edit/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceEntity })
+  async editOcurrence(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+    @Body() { description, level, students }: OcurrenceDto,
+  ) {
+    return await this.ocurrencesService.editOcurrence(
+      parseInt(id),
+      req.user.id,
+      description,
+      level,
+      students,
+    );
+  }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceEntity })
-    async conclueOcurrence(
-        @Param('id') id: string,
-        @Request() req: RequestWithUser,
-    ) {
-        return await this.ocurrencesService.conclueOcurrence(parseInt(id), req.user.role)
-    }
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceEntity })
+  async conclueOcurrence(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+  ) {
+    return await this.ocurrencesService.conclueOcurrence(
+      parseInt(id),
+      req.user.role,
+    );
+  }
 
-    @Delete('/cancel/:id')
-    @UseGuards(JwtAuthGuard)
-    @ApiOkResponse({ type: OcurrenceEntity })
-    async cancelOcurrence(
-        @Param('id') id: string,
-        @Request() req: RequestWithUser,
-    ) {
-        return await this.ocurrencesService.cancelOcurrence(parseInt(id), req.user.id)
-    }
+  @Delete('/cancel/:id')
+  @UseGuards(JwtAuthGuard)
+  @ApiOkResponse({ type: OcurrenceEntity })
+  async cancelOcurrence(
+    @Param('id') id: string,
+    @Request() req: RequestWithUser,
+  ) {
+    return await this.ocurrencesService.cancelOcurrence(
+      parseInt(id),
+      req.user.id,
+    );
+  }
 }
--- src/ocurrences/ocurrences.service.ts
+++ src/ocurrences/ocurrences.service.ts
@@ -1,207 +1,235 @@
-import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Levels } from '@prisma/client';
 import { isAdmin } from 'src/helpers/authorization';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class OcurrencesService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async create(description: string, level: Levels, students: string[], userId) {
-        try {
-            await this.prisma.ocurrence.create({
-                data: {
-                    description: description,
-                    level: level,
-                    userId: userId,
-                    students: {
-                        connect: students.map((ra: string) => ({ ra: ra }))
-                    }
-                }
-            })
-        } catch (e) {
-            console.error(e)
-            throw new BadRequestException('Algo deu errado.')
-        }
+  async create(description: string, level: Levels, students: string[], userId) {
+    try {
+      await this.prisma.ocurrence.create({
+        data: {
+          description: description,
+          level: level,
+          userId: userId,
+          students: {
+            connect: students.map((ra: string) => ({ ra: ra })),
+          },
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async editOcurrence(ocurrenceId: number, userId: number, description: string, level: Levels, students: string[]) {
-        try {
-            const verify = await this.prisma.ocurrence.findUnique({
-                where: {
-                    id: ocurrenceId
-                }
-            })
+  async editOcurrence(
+    ocurrenceId: number,
+    userId: number,
+    description: string,
+    level: Levels,
+    students: string[],
+  ) {
+    try {
+      const verify = await this.prisma.ocurrence.findUnique({
+        where: {
+          id: ocurrenceId,
+        },
+      });
 
-            if (verify.userId != userId) throw new UnauthorizedException("Você não pode execultar essa ação.")
+      if (verify.userId != userId)
+        throw new UnauthorizedException('Você não pode execultar essa ação.');
 
-            const ocurrence = await this.prisma.ocurrence.update({
-                where: {
-                    id: ocurrenceId
-                },
-                data: {
-                    description: description,
-                    level: level,
-                    students: {
-                        set: students.map((ra: string) => ({ ra: ra }))
-                    }
-                },
-                include: {
-                    user: true,
-                    responsible: true,
-                    students: true
-                }
-            })
+      const ocurrence = await this.prisma.ocurrence.update({
+        where: {
+          id: ocurrenceId,
+        },
+        data: {
+          description: description,
+          level: level,
+          students: {
+            set: students.map((ra: string) => ({ ra: ra })),
+          },
+        },
+        include: {
+          user: true,
+          responsible: true,
+          students: true,
+        },
+      });
 
-            return ocurrence
-        } catch (e) {
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return ocurrence;
+    } catch (e) {
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async findOcurrences(userId: number, userRole: string, page: number, limit: number, isArchive: string, queryStudent: string, queryUser: number) {
-        try {
-            const where: any = {
-                ...(isAdmin(userRole)) ? null : { userId: userId },
-                ...(queryStudent && { students: { some: { ra: queryStudent } } }),
-                ...(queryUser && { userId: queryUser }),
-                ...(isArchive === "true" ? {
-                    OR: [
-                        { status: "RESOLVED" },
-                        { status: "CANCELED" }
-                    ]
-                } : {
-                    NOT: {
-                        OR: [
-                            { status: "RESOLVED" },
-                            { status: "CANCELED" }
-                        ]
-                    }
-                })
+  async findOcurrences(
+    userId: number,
+    userRole: string,
+    page: number,
+    limit: number,
+    isArchive: string,
+    queryStudent: string,
+    queryUser: number,
+    queryClass: string,
+  ) {
+    try {
+      const where: any = {
+        ...(isAdmin(userRole) ? null : { userId: userId }),
+        ...(queryStudent && { students: { some: { ra: queryStudent } } }),
+        ...(queryUser && { userId: queryUser }),
+        ...(queryClass && { students: { some: { class: queryClass } } }),
+        ...(isArchive === 'true'
+          ? {
+              OR: [{ status: 'RESOLVED' }, { status: 'CANCELED' }],
             }
+          : {
+              NOT: {
+                OR: [{ status: 'RESOLVED' }, { status: 'CANCELED' }],
+              },
+            }),
+      };
 
-            const total = await this.prisma.ocurrence.count({
-                where: where
-            })
+      const total = await this.prisma.ocurrence.count({
+        where: where,
+      });
 
-            const ocurrences = await this.prisma.ocurrence.findMany({
-                where: where,
-                skip: (page - 1) * limit,
-                take: limit,
-                include: {
-                    students: true,
-                    user: true,
-                    responsible: true
-                }
-            })
+      const ocurrences = await this.prisma.ocurrence.findMany({
+        where: where,
+        skip: (page - 1) * limit,
+        take: limit,
+        include: {
+          students: true,
+          user: true,
+          responsible: true,
+        },
+      });
 
-            return ({ data: ocurrences, meta: { page: page, limit: limit, total: total } })
-        } catch (e) {
-            console.log(e)
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return {
+        data: ocurrences,
+        meta: { page: page, limit: limit, total: total },
+      };
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async assumeOcurrence(ocurrenceId: number, userId: number, userRole: string) {
-        if (!isAdmin(userRole)) throw new UnauthorizedException('Você não pode executar essa ação.')
-        try {
-            const ocurrence = await this.prisma.ocurrence.update({
-                where: {
-                    id: ocurrenceId
-                },
-                data: {
-                    responsibleId: userId,
-                    status: "ASSUMED"
-                },
-                include: {
-                    user: true,
-                    responsible: true,
-                    students: true
-                }
-            })
+  async assumeOcurrence(ocurrenceId: number, userId: number, userRole: string) {
+    if (!isAdmin(userRole))
+      throw new UnauthorizedException('Você não pode executar essa ação.');
+    try {
+      const ocurrence = await this.prisma.ocurrence.update({
+        where: {
+          id: ocurrenceId,
+        },
+        data: {
+          responsibleId: userId,
+          status: 'ASSUMED',
+        },
+        include: {
+          user: true,
+          responsible: true,
+          students: true,
+        },
+      });
 
-            return ocurrence
-        } catch (e) {
-            console.log(e)
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return ocurrence;
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async dispatchOcurrence(ocurrenceId: number, userRole: string, dispatch: string) {
-        if (!isAdmin(userRole)) throw new UnauthorizedException('Você não pode executar essa ação.')
-        try {
-            const ocurrence = await this.prisma.ocurrence.update({
-                where: {
-                    id: ocurrenceId
-                },
-                data: {
-                    dispatch: dispatch,
-                    status: "WAITING"
-                },
-                include: {
-                    user: true,
-                    responsible: true,
-                    students: true
-                }
-            })
+  async dispatchOcurrence(
+    ocurrenceId: number,
+    userRole: string,
+    dispatch: string,
+  ) {
+    if (!isAdmin(userRole))
+      throw new UnauthorizedException('Você não pode executar essa ação.');
+    try {
+      const ocurrence = await this.prisma.ocurrence.update({
+        where: {
+          id: ocurrenceId,
+        },
+        data: {
+          dispatch: dispatch,
+          status: 'WAITING',
+        },
+        include: {
+          user: true,
+          responsible: true,
+          students: true,
+        },
+      });
 
-            return ocurrence
-        } catch (e) {
-            console.log(e)
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return ocurrence;
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async conclueOcurrence(ocurrenceId: number, userRole: string) {
-        if (!isAdmin(userRole)) throw new UnauthorizedException('Você não pode executar essa ação.')
-        try {
-            const ocurrence = await this.prisma.ocurrence.update({
-                where: {
-                    id: ocurrenceId
-                },
-                data: {
-                    status: "RESOLVED"
-                },
-                include: {
-                    user: true,
-                    responsible: true,
-                    students: true
-                }
-            })
+  async conclueOcurrence(ocurrenceId: number, userRole: string) {
+    if (!isAdmin(userRole))
+      throw new UnauthorizedException('Você não pode executar essa ação.');
+    try {
+      const ocurrence = await this.prisma.ocurrence.update({
+        where: {
+          id: ocurrenceId,
+        },
+        data: {
+          status: 'RESOLVED',
+        },
+        include: {
+          user: true,
+          responsible: true,
+          students: true,
+        },
+      });
 
-            return ocurrence
-        } catch (e) {
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return ocurrence;
+    } catch (e) {
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 
-    async cancelOcurrence(ocurrenceId: number, userId: number) {
-        try {
-            const verify = await this.prisma.ocurrence.findUnique({
-                where: {
-                    id: ocurrenceId
-                }
-            })
+  async cancelOcurrence(ocurrenceId: number, userId: number) {
+    try {
+      const verify = await this.prisma.ocurrence.findUnique({
+        where: {
+          id: ocurrenceId,
+        },
+      });
 
-            if (verify.userId != userId) throw new UnauthorizedException('Você não pode execultar essa ação.')
+      if (verify.userId != userId)
+        throw new UnauthorizedException('Você não pode execultar essa ação.');
 
-            const ocurrence = await this.prisma.ocurrence.update({
-                where: {
-                    id: ocurrenceId
-                },
-                data: {
-                    status: "CANCELED"
-                },
-                include: {
-                    user: true,
-                    responsible: true,
-                    students: true
-                }
-            })
+      const ocurrence = await this.prisma.ocurrence.update({
+        where: {
+          id: ocurrenceId,
+        },
+        data: {
+          status: 'CANCELED',
+        },
+        include: {
+          user: true,
+          responsible: true,
+          students: true,
+        },
+      });
 
-            return ocurrence
-        } catch (e) {
-            throw new BadRequestException('Algo deu errado.')
-        }
+      return ocurrence;
+    } catch (e) {
+      throw new BadRequestException('Algo deu errado.');
     }
+  }
 }
